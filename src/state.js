e1ad8236cee4624636b14b4f77a31081c6d91ad7
Abel Kashoba <lebakashoba@hotmail.fr>
2023-03-02 21:39:48 +0100

    Updated the Accounting project url

--- src/state.js
+++ src/state.js
@@ -25,7 +25,7 @@ skills = [
 	{ name:'lambda', src:'psd/awslambda.png' }
 ],
 projects = [
-	{ name:'Accounting', link:'www.testing.com/Accouting', description:"Accounting is a book keeping project that allow you to track your expense and your input to get a better view or picture of your spending pattern.", kind:'html' },
+	{ name:'Accounting', link:'www.testing.com:8080', description:"Accounting is a book keeping project that allow you to track your expense and your input to get a better view or picture of your spending pattern.", kind:'html' },
 	{ name:'Song', link:"www.testing.com/Song", description:"Song is apn App that allow streaming of song Text. It's also a songBook app", kind:'html' }
 ],
 state = {
